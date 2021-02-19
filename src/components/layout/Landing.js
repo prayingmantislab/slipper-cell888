4757b3388c9e90d02af95de78fa84468debed763
Asaf Meir <office@elpuravida.co.il>
2021-02-19 22:21:47 +0200

    added link and switch

---
+++ src/components/layout/Landing.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Sleeper Cell</h1>
+          <p className="lead">
+            Manage your experimenters readings in one place.
+          </p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-light">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
